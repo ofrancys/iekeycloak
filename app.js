2a8aeaa083647c6dd5002aaf9fd31f6cbbb272dc
Francys <francys1010@gmail.com>
2024-12-05 14:02:42 -0300

    updttestes

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
+
+app.use((req, res, next) => {
+  console.log(`Recebendo requisição: ${req.method} ${req.url}`);
+  next();
+});
+
 async function getKeycloakConfig() {
   const response = await axios.get(
     `${process.env.KEYCLOAK_BASE_URL}/realms/${process.env.KEYCLOAK_REALM}/.well-known/openid-configuration`
@@ -16,32 +22,31 @@ async function getKeycloakConfig() {
   };
 }
 
-async function getAccessToken(code) {
-  const response = await axios.post(
-    `${process.env.KEYCLOAK_BASE_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
-    new URLSearchParams({
-      grant_type: "authorization_code",
-      code: code,
-      client_id: process.env.KEYCLOAK_CLIENT_ID,
-      client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
-      redirect_uri: process.env.KEYCLOAK_REDIRECT_URI,
-    }).toString(),
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
+//async function getAccessToken(code) {
+  //const response = await axios.post(
+    //`${process.env.KEYCLOAK_BASE_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
+    //new URLSearchParams({
+      //grant_type: "authorization_code",
+      //code: code,
+      //client_id: process.env.KEYCLOAK_CLIENT_ID,
+      //client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+      //redirect_uri: process.env.KEYCLOAK_REDIRECT_URI,
+    //}).toString(),
+    //{
+      //headers: {
+        //"Content-Type": "application/x-www-form-urlencoded",
+      //},
+   // }
+  //);
+  //if (response.data && response.data.access_token) {
+    //return response.data.access_token;
+  //} else {
+    //console.error("Failed to get access token:", response.data);
+ // }
+//}
 
-  if (response.data && response.data.access_token) {
-    return response.data.access_token;
-  } else {
-    console.error("Failed to get access token:", response.data);
-  }
-}
-
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+//const cookieParser = require("cookie-parser");
+//app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
@@ -100,12 +105,6 @@ app.get("/logout", async (req, res) => {
   res.clearCookie("access_token").redirect(logoutUrl.toString());
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  console.log("Quebrou depois das rotas");
-  res.status(500).send('Quebro aqui');
-});
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -114,4 +113,13 @@ app.listen(PORT, () => {
   console.log("KEYCLOAK_URL:", process.env.KEYCLOAK_BASE_URL);
   console.log("KEYCLOAK_REALM:", process.env.KEYCLOAK_REALM);
   console.log("KEYCLOAK_CLIENT_ID:", process.env.KEYCLOAK_CLIENT_ID);
+});
+
+
+app.listen(3000, () => {
+  console.log("Service Provider running on port 3000");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Service Provider running on port 3000");
 });
