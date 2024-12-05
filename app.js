220672fc814e3e9ea23d2ef2c3da04e65c9a36fc
Francys <francys1010@gmail.com>
2024-12-05 10:01:47 -0300

    limpeza

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-// service-provider/app.js
-
 require("dotenv").config();
 
-// Verificação de variáveis de ambiente
-// const requiredEnvVars = [
-//   'KEYCLOAK_URL',
-//   'KEYCLOAK_REALM',
-//   'KEYCLOAK_CLIENT_ID',
-//   'KEYCLOAK_CLIENT_SECRET'
-// ];
-
-// service-provider/app.js
 const express = require("express");
-// const session = require('express-session');
-// const passport = require('passport');
-// const { Strategy } = require('passport-openidconnect');
-// const crypto = require('crypto');
-
 const axios = require("axios");
 const app = express();
 
@@ -56,71 +40,19 @@ async function getAccessToken(code) {
   }
 }
 
-// Base URLs
-// const EXTERNAL_KEYCLOAK_URL = process.env.KEYCLOAK_URL;
-// const INTERNAL_KEYCLOAK_URL = process.env.KEYCLOAK_URL;
-// const APP_URL = "http://localhost:3001";
-
-// app.use(session({
-//   secret: process.env.SESSION_SECRET || 'chave_sessionSecret',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { secure: false }
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
-app.use(cookieParser);
-
-// Função para gerar PKCE code verifier e challenge
-// function generatePKCE() {
-//   const verifier = crypto.randomBytes(32).toString('base64url');
-//   const challenge = crypto
-//     .createHash('sha256')
-//     .update(verifier)
-//     .digest('base64url');
-//   return { verifier, challenge };
-// }
-
-// Armazenar PKCE em memória (para demonstração)
-// let pkceStore = {};
-
-// const strategyConfig = {
-//   issuer: `${INTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}`,
-//   authorizationURL: `${EXTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/auth`,
-//   tokenURL: `${INTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
-//   userInfoURL: `${INTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/userinfo`,
-//   clientID: process.env.KEYCLOAK_CLIENT_ID,
-//   clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
-//   callbackURL: `${APP_URL}/callback`,
-//   scope: ['openid', 'profile', 'email'],
-//   passReqToCallback: true
-// };
-
-// passport.use('oidc', new Strategy(strategyConfig,
-//   (req, issuer, profile, context, idToken, accessToken, refreshToken, params, done) => {
-//     return done(null, profile);
-// }));
-
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
 
 app.get("/", (req, res) => {
-  // if (req.isAuthenticated()) {
-  // res.send(`
-  //     <h1>Welcome ${req.user.displayName || "User"}</h1>
+  //if (req.isAuthenticated()) {
+  //res.send(`
+  //     <h1>Bem Vindo ${req.user.displayName || "User"}</h1>
   //     <div>
-  //       <h2>User Profile</h2>
+  //      <h2>Perfil do Usuário</h2>
   //       <p>Email: ${
-  //         req.user.emails ? req.user.emails[0].value : "Not available"
-  //       }</p>
+  //       req.user.emails ? req.user.emails[0].value : "Não disponível"
+  //     }</p>
   //       <p>ID: ${req.user.id}</p>
   //       <hr/>
   //       <a href="/logout">Logout</a>
@@ -133,26 +65,14 @@ app.get("/", (req, res) => {
         <a href="/login">Login</a>
       </div>
      `);
+ // }
 });
 
-// app.get('/register', (req, res) => {
-//   const { verifier, challenge } = generatePKCE();
+app.get("/test", (req, res) => {
+  console.log("Rota teste");
+  res.send("Teste");
+});
 
-//   // Armazenar o verifier para uso posterior
-//   const state = crypto.randomBytes(16).toString('hex');
-//   pkceStore[state] = { verifier, challenge };
-
-//   const registrationUrl = new URL(`${EXTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/registrations`);
-//   registrationUrl.searchParams.set('client_id', process.env.KEYCLOAK_CLIENT_ID);
-//   registrationUrl.searchParams.set('response_type', 'code');
-//   registrationUrl.searchParams.set('scope', 'openid email profile');
-//   registrationUrl.searchParams.set('redirect_uri', `${APP_URL}/callback`);
-//   registrationUrl.searchParams.set('state', state);
-//   registrationUrl.searchParams.set('code_challenge', challenge);
-//   registrationUrl.searchParams.set('code_challenge_method', 'S256');
-
-//   res.redirect(registrationUrl.toString());
-// });
 
 app.get("/login", async (req, res, next) => {
   const authUrl = await getKeycloakConfig().authorization_endpoint;
@@ -171,19 +91,6 @@ app.get("/callback", async (req, res, next) => {
   if (!code) return req.status(400).send("Code not found");
   const accessToken = await getAccessToken(code);
   res.cookie("access_token", accessToken).redirect("/");
-  // const state = req.query.state;
-  // const pkceData = pkceStore[state];
-  // if (!pkceData) {
-  //   return res.status(400).send('Invalid state parameter');
-  // }
-  // const { verifier } = pkceData;
-  // passport.authenticate('oidc', {
-  //   code_verifier: verifier,
-  //   successRedirect: '/',
-  //   failureRedirect: '/login'
-  // })(req, res, next);
-  // // Limpar o store após o uso
-  // delete pkceStore[state];
 });
 
 app.get("/logout", async (req, res) => {
@@ -192,6 +99,13 @@ app.get("/logout", async (req, res) => {
   console.info("Redirecting to logout URL:", logout);
   res.clearCookie("access_token").redirect(logoutUrl.toString());
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  console.log("Quebrou depois das rotas");
+  res.status(500).send('Quebro aqui');
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
